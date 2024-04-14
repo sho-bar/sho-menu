f6d8765a778b4622beedb3ce241439e0e43e4bac
SerhiiCho <serhiicho@protonmail.com>
2024-04-14 19:20:24 +0300

    Build release for production

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'sho-bar/sho-menu',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '9a09045f7f9049fb435c06a61bfed5dde8693c65',
+        'reference' => '63ebf718480c8686497e913cdca15d14c3a0bbfc',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -31,7 +31,7 @@
         'sho-bar/sho-menu' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '9a09045f7f9049fb435c06a61bfed5dde8693c65',
+            'reference' => '63ebf718480c8686497e913cdca15d14c3a0bbfc',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
