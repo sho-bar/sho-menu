1b4b947df02b4d9c7d12d909e8e7c18da7fe4f20
SerhiiCho <serhiicho@protonmail.com>
2024-04-17 20:06:02 +0300

    Add nice loading for dishes

---
+++ resources/common/ts/components/DishesLoading.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="sho-menu__dishes__loading">
+        <div
+            v-for="i in 10"
+            :key="i"
+            class="sho-menu__dishes__loading__item"
+        >
+            <div class="sho-menu__dishes__loading__item__content">
+                <h3></h3>
+                <div class="sho-menu__dishes__loading__item__price"></div>
+
+                <div class="sho-menu__dishes__loading__item__description"></div>
+                <div class="sho-menu__dishes__loading__item__description"></div>
+                <div class="sho-menu__dishes__loading__item__description"></div>
+                <div class="sho-menu__dishes__loading__item__description"></div>
+                <div class="sho-menu__dishes__loading__item__description"></div>
+
+                <div class="sho-menu__dishes__loading__item__weight"></div>
+            </div>
+
+            <div class="sho-menu__dishes__loading__item__image"></div>
+        </div>
+    </div>
+</template>
